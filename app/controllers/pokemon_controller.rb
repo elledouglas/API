96d5d8855f1a0c2f4813611235ff80cf0f4a9b12
Kathleen <kathleenmcclay@gmail.com>
2017-08-10 17:01:20 -0400

    Added HTTParty, Pokemon API and GIF Api

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,14 +1,20 @@
 class PokemonController < ApplicationController
-def show
 
-  res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id}/")
+  def show
+
+  res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
   body = JSON.parse(res.body)
   name  = body["name"]
   id = body["id"]
-  type = body["type"]
+  url = body["url"]
+  # id = body["id"]
+  # type = body["type"]
+res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=731ac9dd658f4517bd937b32bbafbec8&q=pikachu&rating=g")
+body = JSON.parse(res.body)
+gif_url = body["data"][0]["url"]
 
-  render json: { "message" : "ok" }
-  #  name.body, id.body, type.body
+render json: { "name": name, "id": id, "url": url }
+
+
 end
-
-end 
+end
